6121c2869bd3f6d49d2efbc68d3418b80de6544c
Victoria Fabrizio <vfabriz1@binghamton.edu>
2018-03-08 14:59:15 -0500

    finished bimodal

--- project1/predictors.cpp
+++ project1/predictors.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <list>
 
 using namespace std;
 
 ifstream input;
 ofstream output;
 
+long address = 0;
+string type;
+
+long correct = 0;
+long total = -1;
+int tableSize = 0;
+
+
 void alwaysTaken() {
-	long address = 0;
-	string type;
 	string prediction = "T";
-	long correct = 0;
-	long total = -1;
+	correct = 0;
+	total = -1;
 
 	while (!input.eof()) {
 		type.clear();
@@ -29,11 +36,8 @@ void alwaysTaken() {
 }
 
 void alwaysNotTaken() {
-	long address = 0;
-	string type;
 	string prediction = "NT";
-	long correct = 0;
-	long total = -1;
+	correct = 0;
 
 	while (!input.eof()) {
 		type.clear();
@@ -43,10 +47,147 @@ void alwaysNotTaken() {
 		if (type == prediction) {
 			correct++;
 		}
-		total++;
 	}
 
 	output << correct << "," << total << ";" << endl;
+}
+
+void checkBimodalSingle(list<string> &table) {
+	list<string>::iterator it = table.begin();
+	correct = 0;
+	input.clear();
+	input.seekg(0, input.beg);
+
+	while (!input.eof()) {
+		type.clear();
+		input >> hex >> address;
+		input >> type;
+
+		address = address % tableSize;
+		it = table.begin();
+		advance(it, address);
+
+		if (type == *it) {
+			correct++;
+		}else {
+			*it = type;
+		}
+	}
+
+	output << correct << "," << total << "; ";
+}
+
+void bimodalSingle() {
+	tableSize = 16;
+	list<string> table (tableSize, "T");
+	checkBimodalSingle(table);
+	
+	tableSize = 32;
+	table.assign(tableSize, "T");
+	checkBimodalSingle(table);
+
+	tableSize = 128;
+	table.assign(tableSize, "T");
+	checkBimodalSingle(table);
+
+	tableSize = 256;
+	table.assign(tableSize, "T");
+	checkBimodalSingle(table);
+
+	tableSize = 512;
+	table.assign(tableSize, "T");
+	checkBimodalSingle(table);
+
+	tableSize = 1024;
+	table.assign(tableSize, "T");
+	checkBimodalSingle(table);
+
+	tableSize = 2048;
+	table.assign(tableSize, "T");
+	checkBimodalSingle(table);
+	
+	output << endl;
+}
+
+void checkBimodalDouble(list<string> &table) {
+	list<string>::iterator it = table.begin();
+	correct = 0;
+	input.clear();
+	input.seekg(0, input.beg);
+
+	while (!input.eof()) {
+		type.clear();
+		input >> hex >> address;
+		input >> type;
+
+		address = address % tableSize;
+		it = table.begin();
+		advance(it, address);
+
+		if (*it == "TT") {
+			if (type == "T") {
+				correct++;
+			}else if (type == "NT") {
+				*it = "WT";
+			}
+		}else if (*it == "WT") {
+			if (type == "T") {
+				correct++;
+				*it = "TT";
+			}else if (type == "NT") {
+				*it = "WN";
+			}
+		}else if (*it == "WN") {
+			if (type == "T") {
+				*it = "WT";
+			}else if (type == "NT") {
+				correct++;
+				*it = "NN";
+			}
+		}else if (*it == "NN") {
+			if (type == "T") {
+				*it = "WN";
+			}else if (type == "NT") {
+				correct++;
+			}
+		}
+	}
+
+	output << correct << "," << total << "; ";
+}
+
+void bimodalDouble() {
+	//TT = strongly taken, WT = weakly taken
+	//WN = weakly not taken, NN = strongly not taken
+	tableSize = 16;
+	list<string> table (tableSize, "TT");
+	checkBimodalDouble(table);
+
+	tableSize = 32;
+	table.assign(tableSize, "TT");
+	checkBimodalDouble(table);
+
+	tableSize = 128;
+	table.assign(tableSize, "TT");
+	checkBimodalDouble(table);
+
+	tableSize = 256;
+	table.assign(tableSize, "TT");
+	checkBimodalDouble(table);
+
+	tableSize = 512;
+	table.assign(tableSize, "TT");
+	checkBimodalDouble(table);
+
+	tableSize = 1024;
+	table.assign(tableSize, "TT");
+	checkBimodalDouble(table);
+
+	tableSize = 2048;
+	table.assign(tableSize, "TT");
+	checkBimodalDouble(table);
+
+	output << endl;
 }
 
 int main(int argc, char *argv[]) {
@@ -69,7 +210,17 @@ int main(int argc, char *argv[]) {
 	input.seekg(0, input.beg);
 	
 	alwaysNotTaken();
+	input.clear();
+	input.seekg(0, input.beg);
 	
+	bimodalSingle();
+	input.clear();
+	input.seekg(0, input.beg);
+
+	bimodalDouble();
+	input.clear();
+	input.seekg(0, input.beg);
+
 	input.close();
 	output.close();
 }
