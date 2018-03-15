f6f136ef7fd51503afcd88d3c7e4ea6658d7ad1b
Victoria Fabrizio <vfabriz1@binghamton.edu>
2018-03-14 23:34:06 -0400

    progress

--- project1/predictors.cpp
+++ project1/predictors.cpp
@@ -2,18 +2,18 @@
 #include <fstream>
 #include <string>
 #include <list>
-#include <map>
+#include "LinkedList.h"
 
 using namespace std;
 
 ifstream input;
 ofstream output;
 
-multimap<long,string> data;
+LinkedList * data = new LinkedList();
 
 long correct = 0;
 long total = -1;
-int tableSize = 0;
+long tableSize = 0;
 
 void readfile() {
 	long address = 0;
@@ -23,150 +23,64 @@ void readfile() {
 		input >> hex >> address;
 		input >> type;
 
-		data.insert(pair<long,string>(address, type));
+		data->insert(address, type);
 		total++;
 	}
 }
 
 void alwaysTaken() {
 	string prediction = "T";
-	correct = 0;
 
-	for (multimap<long,string>::iterator i = data.begin(); i != data.end(); i++) {
-		if (i->second == prediction) {
-			correct++;
-		}
-	}
+	correct = data->iterate(prediction);
 
 	output << correct << "," << total << ";" << endl;
 }
 
 void alwaysNotTaken() {
 	string prediction = "NT";
-	correct = 0;
 
-	for (multimap<long,string>::iterator i = data.begin(); i != data.end(); i++) {
-		if (i->second == prediction) {
-			correct++;
-		}
-	}
+	correct = data->iterate(prediction);
 
 	output << correct << "," << total << ";" << endl;
-}
-
-void checkBimodalSingle(list<string> &table) {
-	list<string>::iterator it = table.begin();
-	correct = 0;
-	long index = 0;
-
-	for (multimap<long,string>::iterator i = data.begin(); i != data.end(); i++) {
-		index = ((*i).first) % tableSize;
-		it = table.begin();
-		advance(it, index);
-
-		if ((*i).second == *it) {
-			correct++;
-		}else {
-			*it = (*i).second;
-		}
-	}
-
-	/*while (!input.eof()) {
-		type.clear();
-		input >> hex >> address;
-		input >> type;
-
-		address = address % tableSize;
-		it = table.begin();
-		advance(it, address);
-
-		if (type == *it) {
-			correct++;
-		}else {
-			*it = type;
-		}
-	}*/
-
-	output << correct << "," << total << "; ";
 }
 
 void bimodalSingle() {
 	tableSize = 16;
 	list<string> table (tableSize, "T");
-	checkBimodalSingle(table);
-	
+	correct = data->iterateBimodalSingle(table, tableSize);
+	output << correct << "," << total << "; ";
+
 	tableSize = 32;
 	table.assign(tableSize, "T");
-	checkBimodalSingle(table);
+	correct = data->iterateBimodalSingle(table, tableSize);
+	output << correct << "," << total << "; ";
 
 	tableSize = 128;
 	table.assign(tableSize, "T");
-	checkBimodalSingle(table);
+	correct = data->iterateBimodalSingle(table, tableSize);
+	output << correct << "," << total << "; ";
 
 	tableSize = 256;
 	table.assign(tableSize, "T");
-	checkBimodalSingle(table);
+	correct = data->iterateBimodalSingle(table, tableSize);
+	output << correct << "," << total << "; ";
 
 	tableSize = 512;
 	table.assign(tableSize, "T");
-	checkBimodalSingle(table);
+	correct = data->iterateBimodalSingle(table, tableSize);
+	output << correct << "," << total << "; ";
 
 	tableSize = 1024;
 	table.assign(tableSize, "T");
-	checkBimodalSingle(table);
+	correct = data->iterateBimodalSingle(table, tableSize);
+	output << correct << "," << total << "; ";
 
 	tableSize = 2048;
 	table.assign(tableSize, "T");
-	checkBimodalSingle(table);
+	correct = data->iterateBimodalSingle(table, tableSize);
+	output << correct << "," << total << "; ";
 	
 	output << endl;
-}
-#if 0
-void checkBimodalDouble(list<string> &table) {
-	list<string>::iterator it = table.begin();
-	correct = 0;
-	input.clear();
-	input.seekg(0, input.beg);
-
-	while (!input.eof()) {
-		type.clear();
-		input >> hex >> address;
-		input >> type;
-
-		address = address % tableSize;
-		it = table.begin();
-		advance(it, address);
-
-		if (*it == "TT") {
-			if (type == "T") {
-				correct++;
-			}else if (type == "NT") {
-				*it = "WT";
-			}
-		}else if (*it == "WT") {
-			if (type == "T") {
-				correct++;
-				*it = "TT";
-			}else if (type == "NT") {
-				*it = "WN";
-			}
-		}else if (*it == "WN") {
-			if (type == "T") {
-				*it = "WT";
-			}else if (type == "NT") {
-				correct++;
-				*it = "NN";
-			}
-		}else if (*it == "NN") {
-			if (type == "T") {
-				*it = "WN";
-			}else if (type == "NT") {
-				correct++;
-			}
-		}
-	}
-
-	output << correct << "," << total << "; ";
 }
 
 void bimodalDouble() {
@@ -174,35 +88,51 @@ void bimodalDouble() {
 	//WN = weakly not taken, NN = strongly not taken
 	tableSize = 16;
 	list<string> table (tableSize, "TT");
-	checkBimodalDouble(table);
+	correct = data->iterateBimodalDouble(table, tableSize);
+	output << correct << "," << total << "; ";
 
 	tableSize = 32;
 	table.assign(tableSize, "TT");
-	checkBimodalDouble(table);
+	correct = data->iterateBimodalDouble(table, tableSize);
+	output << correct << "," << total << "; ";
 
 	tableSize = 128;
 	table.assign(tableSize, "TT");
-	checkBimodalDouble(table);
+	correct = data->iterateBimodalDouble(table, tableSize);
+	output << correct << "," << total << "; ";
 
 	tableSize = 256;
 	table.assign(tableSize, "TT");
-	checkBimodalDouble(table);
+	correct = data->iterateBimodalDouble(table, tableSize);
+	output << correct << "," << total << "; ";
 
 	tableSize = 512;
 	table.assign(tableSize, "TT");
-	checkBimodalDouble(table);
+	correct = data->iterateBimodalDouble(table, tableSize);
+	output << correct << "," << total << "; ";
 
 	tableSize = 1024;
 	table.assign(tableSize, "TT");
-	checkBimodalDouble(table);
+	correct = data->iterateBimodalDouble(table, tableSize);
+	output << correct << "," << total << "; ";
 
 	tableSize = 2048;
 	table.assign(tableSize, "TT");
-	checkBimodalDouble(table);
+	correct = data->iterateBimodalDouble(table, tableSize);
+	output << correct << "," << total << "; ";
 
 	output << endl;
 }
-#endif
+
+void gshare() {
+	tableSize = 2048;
+	list<string> table (tableSize, "TT");
+
+	short int gr = 0;
+	correct = data->iterateGshare(table, tableSize, gr, 3);
+
+
+}
 
 int main(int argc, char *argv[]) {
 	string infilename;
@@ -214,6 +144,7 @@ int main(int argc, char *argv[]) {
 	} 
 	else {
 		cout << "error reading command line" << endl;
+		return 0;
 	}
 
 	input.open(infilename);
@@ -223,13 +154,8 @@ int main(int argc, char *argv[]) {
 	alwaysTaken();
 	alwaysNotTaken();
 	bimodalSingle();
-
-	/*
 	bimodalDouble();
-	input.clear();
-	input.seekg(0, input.beg);
 
 	input.close();
 	output.close();
-	*/
 }
