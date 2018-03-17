b9c06050ca54b81be0057ca52a00f2603f1a1515
Victoria Fabrizio <vfabriz1@binghamton.edu>
2018-03-17 13:07:30 -0400

    everything is done, still 1 off

--- project1/predictors.cpp
+++ project1/predictors.cpp
@@ -4,33 +4,9 @@
 #include <list>
 #include <vector>
 #include <cmath>
+#include <algorithm>
 
 using namespace std;
-
-ifstream input;
-ofstream output;
-
-typedef struct branch {
-	unsigned int address;
-	string type;
-}branch;
-
-vector<branch> data;
-
-unsigned int total = -1;
-
-void readfile() {
-	unsigned int address = 0;
-	string type;
-	while (!input.eof()) {
-		type.clear();
-		input >> hex >> address;
-		input >> type;
-
-		data.push_back({address, type});
-		total++;
-	}
-}
 
 int alwaysTaken(string type) {
 	string prediction = "T";
@@ -48,219 +24,136 @@ int alwaysNotTaken(string type) {
 	return 0;
 }
 
-int bimodalSingle(list<string> &table, unsigned int address, string type) {
-	list<string>::iterator it = table.begin();
-	unsigned int index = 0;
+int bimodalSingle(string * table, int len, unsigned int address, string type) {
+	unsigned int index = address % len;
 
-	index = address % table.size();
-	it = table.begin();
-	std::advance(it, index);
-
-	if (type == *it) {
+	if (type == table[index]) {
 		return 1;
 	}else {
-		*it = type;
+		table[index] = type;
 	}
 	return 0;
 }
 
-int bimodalDouble(list<string> &table, unsigned int address, string type) {
-	list<string>::iterator it = table.begin();
-	unsigned int index = 0;
+int bimodalDouble(string * table, int len, unsigned int address, string type) {
+	
+	unsigned int index = address % len;
 
-	index = address % table.size();
-	it = table.begin();
-	std::advance(it, index);
-
-	if (*it == "TT") {
-		if (type == "T") {
+	if (type == "T") {
+		if (table[index] == "TT") {
 			return 1;
-		}
-		*it = "WT";
-		return 0;
-	}else if (*it == "WT") {
-		if (type == "T") {
-			*it = "TT";
+		} else if (table[index] == "WT") {
+			table[index] = "TT";
 			return 1;
-		}
-		*it = "WN";
-		return 0;
-	}else if (*it == "WN") {
-		if (type == "T") {
-			*it = "WT";
+		} else if (table[index] == "WN") {
+			table[index] = "WT";
 			return 0;
 		}
-		*it = "NN";
-		return 1;
-	}else if (*it == "NN") {
-		if (type == "T") {
-			*it = "WN";
+		table[index] = "WN";
+		return 0;
+	} else {
+		if (table[index] == "TT") {
+			table[index] = "WT";
 			return 0;
+		} else if (table[index] == "WT") {
+			table[index] = "WN";
+			return 0;
+		} else if (table[index] == "WN") {
+			table[index] = "NN";
+			return 1;
 		}
 		return 1;
 	}
 	return 0;
 }
-#if 0
-void iterateGshare(list<string> &table, short int &gr, int grLen) {
-	list<string>::iterator it = table.begin();
-	unsigned int index = 0;
-	correct = 0;
 
-	for (auto &temp : data) {
-		index = (temp.address % tableSize) ^ (gr & (short int)(pow(2, grLen) -1));
-		it = table.begin();
-		std::advance(it, index);
+int gshare(string * table, int len, short int &gr, int grLen, unsigned int address, string type) {
+	
+	unsigned int index = (address % len) ^ (gr & (short int)(pow(2, grLen)-1));
 
-		if (*it == "TT") {
-			if (temp.type == "T") {
-				correct++;
-			}else if (temp.type == "NT") {
-				*it = "WT";
-			}
-		}else if (*it == "WT") {
-			if (temp.type == "T") {
-				correct++;
-				*it = "TT";
-			}else if (temp.type == "NT") {
-				*it = "WN";
-			}
-		}else if (*it == "WN") {
-			if (temp.type == "T") {
-				*it = "WT";
-			}else if (temp.type == "NT") {
-				correct++;
-				*it = "NN";
-			}
-		}else if (*it == "NN") {
-			if (temp.type == "T") {
-				*it = "WN";
-			}else if (temp.type == "NT") {
-				correct++;
-			}
-		}
+	if (type == "T") {
 		//shift gr left one to put in the recent outcome
 		gr = gr << 1;
 		//clear the grLen+1 bit
 		gr &= ~(1 << (grLen+1));
 		//push new into back
-		if (temp.type == "T") {
-			gr ^= 1 << 0;
+		gr ^= 1 << 0;
+		if (table[index] == "TT") {
+			return 1;
+		} else if (table[index] == "WT") {
+			table[index] = "TT";
+			return 1;
+		} else if (table[index] == "WN") {
+			table[index] = "WT";
+			return 0;
 		}
-	}
-}
-
-void gshare() {
-	tableSize = 2048;
-
-	list<string> table (tableSize, "TT");
-	short int gr = 0;
-	iterateGshare(table, gr, 3);
-	output << correct << "," << total << "; ";
-
-	table.assign(tableSize, "TT");
-	gr = 0;
-	iterateGshare(table, gr, 4);
-	output << correct << "," << total << "; ";
-
-	table.assign(tableSize, "TT");
-	gr = 0;
-	iterateGshare(table, gr, 5);
-	output << correct << "," << total << "; ";
-
-	table.assign(tableSize, "TT");
-	gr = 0;
-	iterateGshare(table, gr, 6);
-	output << correct << "," << total << "; ";
-
-	table.assign(tableSize, "TT");
-	gr = 0;
-	iterateGshare(table, gr, 7);
-	output << correct << "," << total << "; ";
-
-	table.assign(tableSize, "TT");
-	gr = 0;
-	iterateGshare(table, gr, 8);
-	output << correct << "," << total << "; ";
-
-	table.assign(tableSize, "TT");
-	gr = 0;
-	iterateGshare(table, gr, 9);
-	output << correct << "," << total << "; ";
-
-	table.assign(tableSize, "TT");
-	gr = 0;
-	iterateGshare(table, gr, 10);
-	output << correct << "," << total << "; ";
-
-	table.assign(tableSize, "TT");
-	gr = 0;
-	iterateGshare(table, gr, 11);
-	output << correct << "," << total << "; ";
-
-	output << endl;
-}
-
-void iterateTournament(list<short int> &selector) {
-	short int gr = 0;
-	int grLen = 11;
-	for (auto &temp : data) {
-		//do gshare
-		index = (temp.address % 2048) ^ (gr & (short int)(pow(2, grLen) -1));
-		it = selector.begin();
-		std::advance(it, index);
-
-		if (*it == "TT") {
-			if (temp.type == "T") {
-				correct++;
-			}else if (temp.type == "NT") {
-				*it = "WT";
-			}
-		}else if (*it == "WT") {
-			if (temp.type == "T") {
-				correct++;
-				*it = "TT";
-			}else if (temp.type == "NT") {
-				*it = "WN";
-			}
-		}else if (*it == "WN") {
-			if (temp.type == "T") {
-				*it = "WT";
-			}else if (temp.type == "NT") {
-				correct++;
-				*it = "NN";
-			}
-		}else if (*it == "NN") {
-			if (temp.type == "T") {
-				*it = "WN";
-			}else if (temp.type == "NT") {
-				correct++;
-			}
-		}
+		table[index] = "WN";
+		return 0;
+	} else {
 		//shift gr left one to put in the recent outcome
 		gr = gr << 1;
 		//clear the grLen+1 bit
 		gr &= ~(1 << (grLen+1));
-		//push new into back
-		if (temp.type == "T") {
-			gr ^= 1 << 0;
+		if (table[index] == "TT") {
+			table[index] = "WT";
+			return 0;
+		} else if (table[index] == "WT") {
+			table[index] = "WN";
+			return 0;
+		} else if (table[index] == "WN") {
+			table[index] = "NN";
+			return 1;
+		}
+		return 1;
+	}
+	return 0;
+}
+
+int tournament(string * grtable, string * bitable, short int * selector, short int &gr, unsigned int address, string type) {
+
+	int bires = bimodalDouble(bitable, 2048, address, type);
+	int grres = gshare(grtable, 2048, gr, 11, address, type);
+
+	if (bires == grres) {
+		if (bires == 1) return 1;
+		else return 0;
+	}
+	else {
+		unsigned int index = address % 2048;
+
+		//0 prefer gshare, 1 weakly prefer gshare
+		//2 weakly prefer bimodal, 3 prefer bimodal
+		if (bires == 1) {
+			if (selector[index] == 0) {
+				selector[index] = 1;
+				return 0;
+			} else if (selector[index] == 1) {
+				selector[index] = 2;
+				return 0;
+			} else if (selector[index] == 2) {
+				selector[index] = 3;
+				return 1;
+			} else {
+				return 1;
+			}
+		} else if (grres == 1) {
+			if (selector[index] == 0) {
+				return 1;
+			} else if (selector[index] == 1) {
+				selector[index] = 0;
+				return 1;
+			} else if (selector[index] == 2) {
+				selector[index] = 1;
+				return 0;
+			} else {
+				selector[index] = 2;
+				return 0;
+			}
 		}
 	}
+	return 0;
 }
 
-void tournament() {
-	//0 prefer gshare, 1 weakly prefer gshare
-	//2 weakly prefer bimodal, 3 prefer bimodal
-	tableSize = 2048;
-	list<string> bimodal (tableSize, "TT");
-	list<string> gshareT (tableSize, "TT");
-	list<short int> selector (tableSize, 0);
-
-	correct = iterateTournament();
-
-	output << correct << "," << total << "; " << endl;
-}
-#endif
 int main(int argc, char *argv[]) {
 	string infilename;
 	string outfilename;
@@ -274,23 +167,33 @@ int main(int argc, char *argv[]) {
 		return 0;
 	}
 
+	ifstream input;
+	ofstream output;
+
 	input.open(infilename);
 	output.open(outfilename);
 
-	readfile();
+	unsigned int total = -1;
 
 	//always
 	int correctAT = 0;
 	int correctNT = 0;
 
 	//bimodal single
-	list<string> tableS16 (16, "T");
-	list<string> tableS32 (32, "T");
-	list<string> tableS128 (128, "T");
-	list<string> tableS256 (256, "T");
-	list<string> tableS512 (512, "T");
-	list<string> tableS1024 (1024, "T");
-	list<string> tableS2048 (2048, "T");
+	string tableS16[16] = {};
+	fill_n(tableS16, 16, "T");
+	string tableS32[32] = {};
+	fill_n(tableS32, 32, "T");
+	string tableS128[128] = {};
+	fill_n(tableS128, 128, "T");
+	string tableS256[256] = {};
+	fill_n(tableS256, 256, "T");
+	string tableS512[512] = {};
+	fill_n(tableS512, 512, "T");
+	string tableS1024[1024] = {};
+	fill_n(tableS1024, 1024, "T");
+	string tableS2048[2048] = {};
+	fill_n(tableS2048, 2048, "T");
 	int correctS16 = 0;
 	int correctS32 = 0;
 	int correctS128 = 0;
@@ -300,13 +203,20 @@ int main(int argc, char *argv[]) {
 	int correctS2048 = 0;
 
 	//bimodal double
-	list<string> tableD16 (16, "TT");
-	list<string> tableD32 (32, "TT");
-	list<string> tableD128 (128, "TT");
-	list<string> tableD256 (256, "TT");
-	list<string> tableD512 (512, "TT");
-	list<string> tableD1024 (1024, "TT");
-	list<string> tableD2048 (2048, "TT");
+	string tableD16[16] = {};
+	fill_n(tableD16, 16, "TT");
+	string tableD32[32] = {};
+	fill_n(tableD32, 32, "TT");
+	string tableD128[128] = {};
+	fill_n(tableD128, 128, "TT");
+	string tableD256[256] = {};
+	fill_n(tableD256, 256, "TT");
+	string tableD512[512] = {};
+	fill_n(tableD512, 512, "TT");
+	string tableD1024[1024] = {};
+	fill_n(tableD1024, 1024, "TT");
+	string tableD2048[2048] = {};
+	fill_n(tableD2048, 2048, "TT");
 	int correctD16 = 0;
 	int correctD32 = 0;
 	int correctD128 = 0;
@@ -315,54 +225,135 @@ int main(int argc, char *argv[]) {
 	int correctD1024 = 0;
 	int correctD2048 = 0;
 
-	for (auto &temp : data) {
-		if (alwaysTaken(temp.type) == 1) {
+	//gshare
+	string tableGr3[2048] = {};
+	fill_n(tableGr3, 2048, "TT");
+	string tableGr4[2048] = {};
+	fill_n(tableGr4, 2048, "TT");
+	string tableGr5[2048] = {};
+	fill_n(tableGr5, 2048, "TT");
+	string tableGr6[2048] = {};
+	fill_n(tableGr6, 2048, "TT");
+	string tableGr7[2048] = {};
+	fill_n(tableGr7, 2048, "TT");
+	string tableGr8[2048] = {};
+	fill_n(tableGr8, 2048, "TT");
+	string tableGr9[2048] = {};
+	fill_n(tableGr9, 2048, "TT");
+	string tableGr10[2048] = {};
+	fill_n(tableGr10, 2048, "TT");
+	string tableGr11[2048] = {};
+	fill_n(tableGr11, 2048, "TT");
+	int correctGr3 = 0;
+	int correctGr4 = 0;
+	int correctGr5 = 0;
+	int correctGr6 = 0;
+	int correctGr7 = 0;
+	int correctGr8 = 0;
+	int correctGr9 = 0;
+	int correctGr10 = 0;
+	int correctGr11 = 0;
+	short int gr3 = 0, gr4 = 0, gr5 = 0, gr6 = 0, gr7 = 0, gr8 = 0, gr9 = 0, gr10 = 0, gr11 = 0;
+
+	//tournament
+	//0 prefer gshare, 1 weakly prefer gshare
+	//2 weakly prefer bimodal, 3 prefer bimodal
+	short int selector[2048] = {};
+	fill_n(selector, 2048, 0);
+	string grtable[2048] = {};
+	fill_n(grtable, 2048, "TT");
+	string bitable[2048] = {};
+	fill_n(bitable, 2048, "TT");
+	int correcttour = 0;
+	short int tourgr11 = 0;
+
+
+	unsigned int address = 0;
+	string type;
+	while (!input.eof()) {
+		type.clear();
+		input >> hex >> address;
+		input >> type;
+
+		total++;
+
+		if (alwaysTaken(type) == 1) {
 			correctAT++;
 		}
-		if (alwaysNotTaken(temp.type) == 1) {
+		if (alwaysNotTaken(type) == 1) {
 			correctNT++;
 		}
-		if (bimodalSingle(tableS16, temp.address, temp.type) == 1) {
+		if (bimodalSingle(tableS16, 16, address, type) == 1) {
 			correctS16++;
 		}
-		if (bimodalSingle(tableS32, temp.address, temp.type) == 1) {
+		if (bimodalSingle(tableS32, 32, address, type) == 1) {
 			correctS32++;
 		}
-		if (bimodalSingle(tableS128, temp.address, temp.type) == 1) {
+		if (bimodalSingle(tableS128, 128, address, type) == 1) {
 			correctS128++;
 		}
-		if (bimodalSingle(tableS256, temp.address, temp.type) == 1) {
+		if (bimodalSingle(tableS256, 256, address, type) == 1) {
 			correctS256++;
 		}
-		if (bimodalSingle(tableS512, temp.address, temp.type) == 1) {
+		if (bimodalSingle(tableS512, 512, address, type) == 1) {
 			correctS512++;
 		}
-		if (bimodalSingle(tableS1024, temp.address, temp.type) == 1) {
+		if (bimodalSingle(tableS1024, 1024, address, type) == 1) {
 			correctS1024++;
 		}
-		if (bimodalSingle(tableS2048, temp.address, temp.type) == 1) {
+		if (bimodalSingle(tableS2048, 2048, address, type) == 1) {
 			correctS2048++;
 		}
-		if (bimodalDouble(tableD16, temp.address, temp.type) == 1) {
+		if (bimodalDouble(tableD16, 16, address, type) == 1) {
 			correctD16++;
 		}
-		if (bimodalDouble(tableD32, temp.address, temp.type) == 1) {
+		if (bimodalDouble(tableD32, 32, address, type) == 1) {
 			correctD32++;
 		}
-		if (bimodalDouble(tableD128, temp.address, temp.type) == 1) {
+		if (bimodalDouble(tableD128, 128, address, type) == 1) {
 			correctD128++;
 		}
-		if (bimodalDouble(tableD256, temp.address, temp.type) == 1) {
+		if (bimodalDouble(tableD256, 256, address, type) == 1) {
 			correctD256++;
 		}
-		if (bimodalDouble(tableD512, temp.address, temp.type) == 1) {
+		if (bimodalDouble(tableD512, 512, address, type) == 1) {
 			correctD512++;
 		}
-		if (bimodalDouble(tableD1024, temp.address, temp.type) == 1) {
+		if (bimodalDouble(tableD1024, 1024, address, type) == 1) {
 			correctD1024++;
 		}
-		if (bimodalDouble(tableD2048, temp.address, temp.type) == 1) {
+		if (bimodalDouble(tableD2048, 2048, address, type) == 1) {
 			correctD2048++;
+		}
+		if (gshare(tableGr3, 2048, gr3, 3, address, type) == 1) {
+			correctGr3++;
+		}
+		if (gshare(tableGr4, 2048, gr4, 4, address, type) == 1) {
+			correctGr4++;
+		}
+		if (gshare(tableGr5, 2048, gr5, 5, address, type) == 1) {
+			correctGr5++;
+		}
+		if (gshare(tableGr6, 2048, gr6, 6, address, type) == 1) {
+			correctGr6++;
+		}
+		if (gshare(tableGr7, 2048, gr7, 7, address, type) == 1) {
+			correctGr7++;
+		}
+		if (gshare(tableGr8, 2048, gr8, 8, address, type) == 1) {
+			correctGr8++;
+		}
+		if (gshare(tableGr9, 2048, gr9, 9, address, type) == 1) {
+			correctGr9++;
+		}
+		if (gshare(tableGr10, 2048, gr10, 10, address, type) == 1) {
+			correctGr10++;
+		}
+		if (gshare(tableGr11, 2048, gr11, 11, address, type) == 1) {
+			correctGr11++;
+		}
+		if (tournament(grtable, bitable, selector, tourgr11, address, type) == 1) {
+			correcttour++;
 		}
 	}
 
@@ -387,9 +378,19 @@ int main(int argc, char *argv[]) {
 	output << correctD2048 << "," << total << "; ";
 	output << endl;
 
+	output << correctGr3 << "," << total << "; ";
+	output << correctGr4 << "," << total << "; ";
+	output << correctGr5 << "," << total << "; ";
+	output << correctGr6 << "," << total << "; ";
+	output << correctGr7 << "," << total << "; ";
+	output << correctGr8 << "," << total << "; ";
+	output << correctGr9 << "," << total << "; ";
+	output << correctGr10 << "," << total << "; ";
+	output << correctGr11 << "," << total << "; ";
+	output << endl;
 
-	//bimodalDouble();
-	//gshare();
+	output << correcttour << "," << total << "; " << endl;
+
 	//tournament();
 
 	input.close();
