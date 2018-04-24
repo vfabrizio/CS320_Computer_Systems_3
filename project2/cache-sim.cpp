7de67fe8924f1d0ba4948e709ad1b18dfc0f1af2
Victoria Fabrizio <vfabriz1@binghamton.edu>
2018-04-24 13:13:06 -0400

    making progress

--- project2/cache-sim.cpp
+++ project2/cache-sim.cpp
@@ -2,9 +2,9 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <deque>
 #include <cmath>
 #include <utility> //pair
-#include "LinkedList.h"
 
 using namespace std;
 
@@ -31,7 +31,7 @@ int direct_mapped(unsigned int address, pair<short int, int> table[], int size) 
 	return 0;
 }
 
-int set_associative(unsigned int address, pair<short int, int> table[], vector<LinkedList> list, int ways) {
+int set_associative(unsigned int address, pair<short int, int> table[], vector<deque<pair<int,int> > > list, int ways) {
 	//all tables are 512 but if in another set then we will check
 	//		index + num of lines
 	int num_lines = 512 / ways;
@@ -51,18 +51,37 @@ int set_associative(unsigned int address, pair<short int, int> table[], vector<L
 		index = index+num_lines;
 	}
 	//didn't find in any way
-	if (list.empty()) {
+	if (list[tempindex].front().second == 0) {
+		//first element has way value = 0 so there's nothing there
 		table[tempindex].first = 1;
 		table[tempindex].second = tag;
-		list.insert(list.begin(), tag);
-	} else if (list.size() < num_lines) {
-		list.insert(list.begin(), tag);
-		int way_num = list.size();
+		list[tempindex].pop_back();
+		list[tempindex].push_front(make_pair(tag, 1));
+	} else if (list[tempindex].back().second == 0) {
+		//if the last way is 0 then there's still room
+		int i = 0;
+		while (i < ways) {
+			if (list[tempindex][i].second != 0) i++;
+			else break;
+		}
+		int way_num = i;
+		list[tempindex].pop_back();
+		list[tempindex].push_front(make_pair(tag, way_num));
 		table[tempindex + (way_num*num_lines)].first = 1;
 		table[tempindex + (way_num*num_lines)].second = tag;
-	} else if (list.size() == ways) {
-		
+	} else {
+		//returns last element in LL == LRU
+		int way = list[tempindex].back().second;
+		int oldTag = list[tempindex].back().first;
+		list[tempindex].pop_back();
+		list[tempindex].push_front(make_pair(tag, way)); //new way = old way
+		table[tempindex + (way*num_lines)].first = 1;
+		table[tempindex + (way*num_lines)].second = tag;
 	}
+	return 0;
+}
+
+int fully_associative() {
 
 }
 
@@ -104,22 +123,27 @@ int main(int argc, char *argv[]) {
 
 	//set-associative
 	//all 16kb == 512 lines
+	//pair in array <tag, way>
 	pair<short int, int> twoset[512] = {};
 	fill_n(twoset, 512, make_pair(0, 0));
-	vector<LinkedList> two;
+	vector<deque<pair<int,int> > > two(256, deque<pair<int,int> >(2, make_pair(0,0)));
+
 	pair<short int, int> fourset[512] = {};
 	fill_n(fourset, 512, make_pair(0, 0));
-	vector<LinkedList> four;
+	vector<deque<pair<int,int> > > four(128, deque<pair<int,int> >(4, make_pair(0,0)));
+
 	pair<short int, int> eightset[512] = {};
 	fill_n(eightset, 512, make_pair(0, 0));
-	vector<LinkedList> eight;
+	vector<deque<pair<int,int> > > eight(64, deque<pair<int,int> >(8, make_pair(0,0)));
+
 	pair<short int, int> sixteenset[512] = {};
 	fill_n(sixteenset, 512, make_pair(0, 0));
-	vector<LinkedList> sixteen;
-	int correct2 = 0;
-	int correct4 = 0;
-	int correct8 = 0;
-	int correct16 = 0;
+	vector<deque<pair<int,int> > > sixteen(32, deque<pair<int,int> >(16, make_pair(0,0)));
+
+	int correctSA2 = 0;
+	int correctSA4 = 0;
+	int correctSA8 = 0;
+	int correctSA16 = 0;
 
 	unsigned int address = 0;
 	string type; //type is L = load or S = store
@@ -147,16 +171,16 @@ int main(int argc, char *argv[]) {
 			correct32++;
 		}
 		if (set_associative(address, twoset, two, 2) == 1) {
-			correct2++;
+			correctSA2++;
 		}
 		if (set_associative(address, fourset, four, 4) == 1) {
-			correct4++;
+			correctSA4++;
 		}
 		if (set_associative(address, eightset, eight, 8) == 1) {
-			correct8++;
+			correctSA8++;
 		}
 		if (set_associative(address, sixteenset, sixteen, 16) == 1) {
-			correct16++;
+			correctSA16++;
 		}
 
 	}
@@ -165,4 +189,9 @@ int main(int argc, char *argv[]) {
 	output << correct4 << "," << total << "; ";
 	output << correct16 << "," << total << "; ";
 	output << correct32 << "," << total << ";" << endl;
+
+	output << correctSA2 << "," << total << "; ";
+	output << correctSA4 << "," << total << "; ";
+	output << correctSA8 << "," << total << "; ";
+	output << correctSA16 << "," << total << ";" << endl;
 }
